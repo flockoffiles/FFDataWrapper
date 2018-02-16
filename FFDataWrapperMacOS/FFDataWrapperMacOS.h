c2ed58f8e3cfe55525abc68b9b173098a72a4ac2
Sergey A. Novitsky <sergey.novitsky@ing.nl>
2018-02-16 15:09:27 +0100

    Added a MacOS framework target

---
+++ FFDataWrapperMacOS/FFDataWrapperMacOS.h
@@ -0,0 +1,19 @@
+//
+//  FFDataWrapperMacOS.h
+//  FFDataWrapperMacOS
+//
+//  Created by Sergey Novitsky on 16/02/2018.
+//  Copyright © 2018 Flock of Files. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FFDataWrapperMacOS.
+FOUNDATION_EXPORT double FFDataWrapperMacOSVersionNumber;
+
+//! Project version string for FFDataWrapperMacOS.
+FOUNDATION_EXPORT const unsigned char FFDataWrapperMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFDataWrapperMacOS/PublicHeader.h>
+
+
